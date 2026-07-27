caa3c883d2a501ccc64d6f7431ac87b1b9628f20
Anukalp Agarwal <anukalpagarwal24@gmail.com>
2026-07-27 20:05:06 +0530

    Migrate database connection to MongoDB Atlas

--- Backend/database/mongoose.js
+++ Backend/database/mongoose.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose")
 const connectDB  =  async ()=>{
   try {
-    await mongoose.connect(`mongodb://127.0.0.1:27017/airbnb`)
-
+    await mongoose.connect(process.env.MONGODB_URI)
      console.log("Mongooses is connected baby 😄 ")
   } catch (error) {
     console.log(error)
