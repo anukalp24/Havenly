5d44756c62b643998ec2eaaa75e92dfe1b2334d6
Anukalp Agarwal <anukalpagarwal24@gmail.com>
2026-07-28 20:24:27 +0530

    imported jwt

--- Backend/Controllers/Auth/signinController.js
+++ Backend/Controllers/Auth/signinController.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcryptjs");
 const crypto = require("crypto")
 const { Resend } = require("resend");
 const resend = new Resend(process.env.RESEND_API_KEY);
-
+const jwt = require("jsonwebtoken")
 
 
 
